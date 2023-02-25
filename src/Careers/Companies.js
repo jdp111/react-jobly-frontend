145d45c846e1a501520ef0c939867d01db73ee55
jdp111 <jdpearce111@gmail.com>
2023-02-25 03:41:21 -0700

    Companies with searchability

--- src/Careers/Companies.js
+++ src/Careers/Companies.js
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from "react"
 import JoblyApi from "../api"
 import { Card, CardBody, CardHeader } from "reactstrap";
 import { NavLink } from "react-router-dom";
+import SearchBar from "./SearchBar";
 
 
 function Companies() {
 
-    const [searchTerm, setSearchTerm] = useState(null)
+    const [searchTerm, setSearchTerm] = useState("")
     const [comps, setComps] = useState(false)
 
 
@@ -14,21 +15,21 @@ function Companies() {
     useEffect(()=>{
         async function getCompanies() {
             const companyList = await JoblyApi.getCompanies(searchTerm)
-            console.log("companies",companyList.companies)
             return companyList.companies
         }
         getCompanies().then((res)=>{
             setComps(res);
         })
-    },[])
+    },[searchTerm])
 
 
     return (
         <div>
             <h3>All Companies</h3>
+            <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
             {comps && comps.map((company)=>(
-                <NavLink className="jobnav" exact to={`/companies/${company.handle}`}>
-                <Card className="job" key = {company.handle}>
+                <NavLink key = {company.handle} className="jobnav" to={`/companies/${company.handle}`}>
+                <Card className="job" >
                     <CardHeader>
                     <h6>{company.name}</h6>
                     </CardHeader>
--- src/Careers/SearchBar.js
+++ src/Careers/SearchBar.js
@@ -1,22 +1,30 @@
-import { useState } from "react"
-import Companies from "./Companies"
+import { Card, CardBody, CardHeader } from "reactstrap";
 
 
+function SearchBar({searchTerm, setSearchTerm}) {
 
-function Postings() {
+    // handles search term change on submit
+    const handleChange = (evt)=>{
+        const {name, value} = evt.target
+        setSearchTerm(value)
+    }
 
-    const [searchTerm, setSearchTerm] = useState(null)
+    const handleSubmit = (evt)=>{
+        evt.preventDefault()
+    }
+
+
 
     return(
-        <div>
-            <form>
-                <input></input>
+        <div className="search-box form-outline">
+            <form onSubmit={handleSubmit}>
+                <label className="form-label" htmlFor="search" >Search:</label>
+                <input className="form-control" onChange={handleChange} name= "search" value = {searchTerm} type="text" placeholder="search anything!"></input>
             </form>
 
-            <Companies searchTerm = {searchTerm}/>
         </div>
     )
 
 }
 
-export default Postings
+export default SearchBar
